c43c63d87196fd750515186cb9f516cb023fa3c1
Poweii <23715831.vy@student.iuh.edu.vn>
2025-05-05 13:34:30 +0700

    Update main.py

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,10 +1,22 @@
-import boto3
+import requests
+import gzip
+from io import BytesIO
 
-
+def get_file(base,key):
+    url = base + key
+    resp = requests.get(url,stream = True)
+    resp.raise_for_status()
+    return resp
 def main():
-    # your code here
-    pass
+    s3_base = "https://data.commoncrawl.org/"
+    key = "crawl-data/CC-MAIN-2022-05/wet.paths.gz"
+    resp_wet = get_file(s3_base,key)
+    with gzip.open(BytesIO(resp_wet.content), 'rt', encoding='utf-8') as f: 
+        uri_path = f.readline().strip()
 
-
+    resp_warc = get_file(s3_base,uri_path)
+    for line in resp_warc.iter_lines(decode_unicode=True):
+        print(line)
+        
 if __name__ == "__main__":
     main()
